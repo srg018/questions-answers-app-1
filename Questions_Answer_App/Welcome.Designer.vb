b2720118f05ad6d9f87b89a9ceddfbdb660570b0
Shannon Gordon <srg018@uark.edu>
2017-10-19 08:47:25 -0500

    Add questions datagridview

--- Questions_Answer_App/Welcome.Designer.vb
+++ Questions_Answer_App/Welcome.Designer.vb
@@ -22,17 +22,59 @@ Partial Class Welcome
 	'Do not modify it using the code editor.
 	<System.Diagnostics.DebuggerStepThrough()> _
 	Private Sub InitializeComponent()
+		Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+		Me.LoadToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+		Me.DGVQuestions = New System.Windows.Forms.DataGridView()
+		Me.MenuStrip1.SuspendLayout()
+		CType(Me.DGVQuestions, System.ComponentModel.ISupportInitialize).BeginInit()
 		Me.SuspendLayout()
+		'
+		'MenuStrip1
+		'
+		Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.LoadToolStripMenuItem})
+		Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+		Me.MenuStrip1.Name = "MenuStrip1"
+		Me.MenuStrip1.Size = New System.Drawing.Size(848, 24)
+		Me.MenuStrip1.TabIndex = 0
+		Me.MenuStrip1.Text = "MenuStrip1"
+		'
+		'LoadToolStripMenuItem
+		'
+		Me.LoadToolStripMenuItem.Name = "LoadToolStripMenuItem"
+		Me.LoadToolStripMenuItem.Size = New System.Drawing.Size(45, 20)
+		Me.LoadToolStripMenuItem.Text = "Load"
+		'
+		'DGVQuestions
+		'
+		Me.DGVQuestions.AllowUserToAddRows = False
+		Me.DGVQuestions.AllowUserToDeleteRows = False
+		Me.DGVQuestions.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+		Me.DGVQuestions.Dock = System.Windows.Forms.DockStyle.Fill
+		Me.DGVQuestions.Location = New System.Drawing.Point(0, 24)
+		Me.DGVQuestions.Name = "DGVQuestions"
+		Me.DGVQuestions.ReadOnly = True
+		Me.DGVQuestions.Size = New System.Drawing.Size(848, 440)
+		Me.DGVQuestions.TabIndex = 1
 		'
 		'Welcome
 		'
 		Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
 		Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-		Me.ClientSize = New System.Drawing.Size(284, 261)
+		Me.ClientSize = New System.Drawing.Size(848, 464)
+		Me.Controls.Add(Me.DGVQuestions)
+		Me.Controls.Add(Me.MenuStrip1)
+		Me.MainMenuStrip = Me.MenuStrip1
 		Me.Name = "Welcome"
 		Me.Text = "Welcome"
+		Me.MenuStrip1.ResumeLayout(False)
+		Me.MenuStrip1.PerformLayout()
+		CType(Me.DGVQuestions, System.ComponentModel.ISupportInitialize).EndInit()
 		Me.ResumeLayout(False)
+		Me.PerformLayout()
 
 	End Sub
 
+	Friend WithEvents MenuStrip1 As MenuStrip
+	Friend WithEvents LoadToolStripMenuItem As ToolStripMenuItem
+	Friend WithEvents DGVQuestions As DataGridView
 End Class
